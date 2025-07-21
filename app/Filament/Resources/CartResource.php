eb133f628e2b94abf6cc30ffafd3fc9ec8a9433b
Elcand <tjandraraharja99@gmail.com>
2025-07-21 15:20:20 +0700

    feat: show data carts git push origin carts

--- app/Filament/Resources/CartResource.php
+++ app/Filament/Resources/CartResource.php
@@ -6,9 +6,11 @@ use App\Filament\Resources\CartResource\Pages;
 use App\Filament\Resources\CartResource\RelationManagers;
 use App\Models\Cart;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -33,13 +35,20 @@ class CartResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('product.title')
+                    ->searchable(),
+                TextColumn::make('customer.name')
+                    ->searchable(),
+                TextColumn::make('qty'),
+                TextColumn::make('total')
+                    ->money('IDR', locale: 'id')
+                    ->getStateUsing(fn($record) => $record->qty * $record->price),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
