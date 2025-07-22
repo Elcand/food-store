fab112bfec0220b9a4b5676e1ae7291221ec6b9a
Elcand <tjandraraharja99@gmail.com>
2025-07-22 11:29:04 +0700

    feat: show data user

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -11,6 +11,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -53,7 +54,9 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('email')->searchable(),
+                TextColumn::make('created_at')->dateTime(),
             ])
             ->filters([
                 //
