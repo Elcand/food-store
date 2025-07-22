eca1f6bd147c49716167cf7afc28cef80fe7558d
Elcand <tjandraraharja99@gmail.com>
2025-07-22 15:42:12 +0700

    feat: make component register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace App\Livewire;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', Auth\Register::class)->name('register');
