5e1b9ad1f72f7ade26f5db3c035760d2841c4b80
Elcand <tjandraraharja99@gmail.com>
2025-07-24 10:31:32 +0700

    feat: show popular products

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -3,16 +3,27 @@
 namespace App\Livewire\Web\Home;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slider;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public function getPopularProducts()
+    {
+        return Product::with('category', 'ratings.customer')
+            ->withAvg('ratings', 'rating')
+            ->having('ratings_avg_rating', '>=', 4)
+            ->limit(5)
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.web.home.index', [
-            'sliders'    => Slider::latest()->get(),
-            'categories' => Category::latest()->get(),
+            'sliders'           => Slider::latest()->get(),
+            'categories'        => Category::latest()->get(),
+            'popularProducts'   => $this->getPopularProducts(),
         ]);
     }
 }
