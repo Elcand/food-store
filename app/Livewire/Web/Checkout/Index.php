5b0a66b36d98a5275eba18d0ab20225af177b3b0
Elcand <tjandraraharja99@gmail.com>
2025-07-26 09:41:38 +0700

    feat: calculating shipping costs

--- app/Livewire/Web/Checkout/Index.php
+++ app/Livewire/Web/Checkout/Index.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Web\Checkout;
 
+use App\Models\Cart;
 use App\Models\Province;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Index extends Component
@@ -10,12 +12,102 @@ class Index extends Component
     public $address;
     public $province_id;
     public $city_id;
-    public $disctict_id;
+    public $district_id;
+
+    public $loading  = false;
+    public $showCost = false;
+    public $costs;
+
+    public $selectCourier = '';
+    public $selectService = '';
+    public $selectCost = 0;
+
+    public $grandTotal = 0;
+
+    public function getCartsData()
+    {
+        $carts = Cart::query()
+            ->with('product')
+            ->where('customer_id', auth()->guard('customer')->user()->id)
+            ->latest()
+            ->get();
+
+        $totalWeight = $carts->sum(function ($cart) {
+            return $cart->product->weight * $cart->qty;
+        });
+
+        $totalPrice = $carts->sum(function ($cart) {
+            return $cart->product->price * $cart->qty;
+        });
+
+        return [
+            'totalWeight' => $totalWeight,
+            'totalPrice'  => $totalPrice,
+        ];
+    }
+
+    public function changeCourier($value)
+    {
+        if (!empty($value)) {
+            $this->selectCourier = $value;
+
+            $this->loading = true;
+
+            $this->showCost = false;
+
+            $this->CheckOngkir();
+        }
+    }
+
+
+    public function CheckOngkir()
+    {
+        try {
+            $cartData = $this->getCartsData();
+
+            $response = Http::withHeaders([
+                'Accept' => 'application/json',
+                'key'    => config('rajaongkir.api_key'),
+            ])->withOptions([
+                'query' => [
+                    'origin'      => 3855,
+                    'destination' => $this->district_id,
+                    'weight'      => $cartData['totalWeight'],
+                    'courier'     => $this->selectCourier,
+                ]
+            ])->post('https://rajaongkir.komerce.id/api/v1/calculate/domestic-cost');
+
+            $this->costs = $response->json()['data'];
+        } catch (\Exception $e) {
+            session()->flash('error', 'Gagal mengambil ongkir: ' . $e->getMessage());
+        } finally {
+            $this->loading = false;
+            $this->showCost = true;
+        }
+    }
+
+    public function getServiceAndCost($data)
+    {
+        [$cost, $service] = explode('|', $data);
+
+        $this->selectCost = (int) $cost;
+        $this->selectService = $service;
+
+        $cartData = $this->getCartsData();
+
+        $this->grandTotal = $cartData['totalPrice'] + $this->selectCost;
+    }
+
+
 
     public function render()
     {
         $provinces = Province::query()->get();
 
-        return view('livewire.web.checkout.index', compact('provinces'));
+        $cartData = $this->getCartsData();
+        $totalPrice     = $cartData['totalPrice'];
+        $totalWeight    = $cartData['totalWeight'];
+
+        return view('livewire.web.checkout.index', compact('provinces', 'totalPrice', 'totalWeight'));
     }
 }
